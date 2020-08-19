275b39ca6c3042ce14312b60351857accaf39c71
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-08-19 09:18:43 -0700

    User login post route

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,9 +1,33 @@
-const MongoClient=require("mongodb").MongoClient;
-const assert=require('assert')
-const {mongoKey}=require('./credentials')
+const MongoClient = require("mongodb").MongoClient;
+// const assert = require("assert");
+const { mongoKey, dbName } = require("./credentials");
+const db = (callback) =>
+  MongoClient.connect(
+    mongoKey,
+    { useUnifiedTopology: true },
+    (error, client) => {
+        if(error){
+            callback(null,error)
+        }
+      console.log("Connected successfully to server");
+      callback(client.db(dbName).collection("users"));
+    }
+  )
 
-MongoClient.connect(mongoKey,{ useUnifiedTopology: true },(error,client)=>{
-    assert.equal(null, error);
-    console.log("Connected successfully to server");
-    console.log(client)
-})
+// db((mydb,error)=>{  // fetching database using callback
+//     console.log(mydb)
+// })
+
+module.exports = {
+  db
+};
+
+// collection.insertOne({
+//     email:'shresthashare@gmail.com',
+//     password:'test12345'
+// },(error,result)=>{
+//     assert.equal(null,error)
+//     assert.equal(1, result.result.n);
+//     assert.equal(1, result.ops.length);
+//     console.log("Inserted document into the collection");
+// })
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const cors=require('cors')
 const app=express()
 const PORT=process.env.PORT || 5000
-
+const {db}=require('./db/db')
 
 app.use(cors())
 app.use(express.json())
@@ -10,6 +10,34 @@ app.use(express.json())
 app.get('/',(request,response)=>{
     response.send("Backend is working.")
 })
+
+app.post('/login',(request,response)=>{
+    const email=request.body.email
+    const password=request.body.password
+    if(!email || !password){
+        return response.status(400).send({error:'Invalid Fields'})
+    }
+    db((collection,error)=>{
+        if(error){
+            return response.status(500).send({error:'Server Error'})
+        }
+        collection.findOne({email,password},(error,{email})=>{
+            if(error){
+                return response.status(400).send({error:'Invalid Credentials'})
+            }
+            return response.status(200).send({email})
+        })
+    })
+
+
+    // database.findOne({email,password},(error,result)=>{
+    //     if(!error){
+    //         console.log(result);
+    //     }
+    //     return
+    // })
+})
+
 
 app.listen(PORT,()=>{
     console.log("Bakend started on port "+PORT);
