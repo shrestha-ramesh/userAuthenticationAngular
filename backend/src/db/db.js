cfc79ab5ad0d4a13b8321fb877b685db4fc31b9c
Suraj Shrestha <shresthasuraj62@gmail.com>
2020-08-18 23:12:19 -0700

    Added db.js for mongoDB connection

---
+++ backend/src/db/db.js
@@ -0,0 +1,10 @@
+const MongoClient=require("mongodb").MongoClient;
+const assert=require('assert')
+const {mongoKey}=require('./credentials')
+
+MongoClient.connect(mongoKey,{ useUnifiedTopology: true },(error,client)=>{
+    // assert.equal(null, error);
+    console.log("Connected successfully to server");
+    console.log(error.message);
+    console.log(client)
+})
